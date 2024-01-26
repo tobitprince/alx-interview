41798acb06f74cb3862aa60e46e45abfd2503edf
tobitprince <prince.tobit@strathmore.edu>
2024-01-26 03:46:57 +0300

    Refactor log parsing script to improve performance and readability

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,36 +3,35 @@
 
 
 import sys
-import re
 
-
-def print_stats(size: int, status_code: dict) -> None:
-    """Print stats."""
-    print("File size: {}".format(size))
-    for key, value in sorted(status_code.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
-
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
 counter = 0
-size = 0
-status_code = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
 
 try:
     for line in sys.stdin:
-        match = re.match(
-            r"(.*) - \[.*\] \"GET /projects/260 HTTP/1.1\" (\d+) (\d+)",
-            line)
-        if match:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
             counter += 1
-            size += int(match.group(3))
-            if match.group(2) in status_code:
-                status_code[match.group(2)] += 1
 
         if counter == 10:
-            print_stats(size, status_code)
             counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
 
 finally:
-    print_stats(size, status_code)
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
