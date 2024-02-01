234c379931582fe51422839dd7bd830f884d5087
tobitprince <prince.tobit@strathmore.edu>
2024-02-01 10:06:18 +0300

    Refactor validUTF8 function to validutf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,8 +2,8 @@
 """Module for validUTF8 function."""
 
 
-def validUTF8(data):
-    """Return: True if data is a valid UTF-8 encoding, else return False."""
+def validutf8(data):
+    """Return true if data is a valid UTF-8 encoding, else return false."""
     try:
         bytes(data).decode('utf-8')
         return True
